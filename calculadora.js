b70ea0dd3ea65eaf758759c7463e893748f5f033
Vinicius Sueiro <hi@vsueiro.com>
2022-09-19 09:30:16 -0400

    Adiciona filtro de gênero e calcula chance de ser eleito

--- calculadora.js
+++ calculadora.js
@@ -6,7 +6,10 @@ function filtrar() {
 
     // Pega valor mais atual do seletor de cor/raça
     let corRaca = seletorCorRaca.value
+    let genero = seletorGenero.value
 
+    // Declara variável para ser filtrada múltiplas vezes
+    let dadosFiltrados
 
     function checarCorRaca(dado) {
 
@@ -19,34 +22,62 @@ function filtrar() {
         return dado.DS_COR_RACA === corRaca
     }
 
+    function checarGenero(dado) {
+
+        // Evita filtrar se opção for “Todos”
+        if (genero === 'TODOS') {
+            return true
+        }
+
+        // Mantém apenas as pessoas que tem o gênero igual à opção selecionada
+        return dado.DS_GENERO === genero
+    }
+
     // Filtra dados
-    let dadosFiltrados = dados.filter(checarCorRaca)
+    dadosFiltrados = dados.filter(checarCorRaca)
+    dadosFiltrados = dadosFiltrados.filter(checarGenero)
+
+    function filtrarEleitos(dado) {
+        return dado.DS_SIT_TOT_TURNO === 'ELEITO'
+    }
+
+    // Filtra por pessoas eleitas
+    let pessoasEleitas = dadosFiltrados.filter(filtrarEleitos)
 
     // Atualiza lista com dados filtrados
-    mostrar(dadosFiltrados)
+    mostrar(dadosFiltrados, pessoasEleitas)
 
 }
 
 // Quando pessoa seleciona uma cor/raça
 seletorCorRaca.onchange = filtrar
+seletorGenero.onchange = filtrar
 
 // Define função para mostrar pessoas candidatas
-function mostrar(dados) {
+function mostrar(candidatos, eleitos) {
 
     // Atualiza total
-    total.textContent = dados.length
+    total.textContent = candidatos.length
+
+    // Atualiza total de eleitos
+    totalEleitos.textContent = eleitos.length
+
+    let valorPercentual = eleitos.length / candidatos.length * 100
+
+    // Calcula percentual
+    percentual.textContent = valorPercentual.toFixed(1) + '%'
 
     // Limpa a lista
     nomes.textContent = ''
 
     // Adiciona itens
-    for (let dado of dados) {
+    for (let candidato of candidatos) {
 
         // Cria elemento <li>
         li = document.createElement('li')
 
         // Adiciona nome de urna dentro da <li>
-        li.textContent = dado.NM_URNA_CANDIDATO
+        li.textContent = candidato.NM_URNA_CANDIDATO
 
         // Adiciona <li> à <ul>
         nomes.append(li)
@@ -58,7 +89,7 @@ async function carregar() {
     dados = await d3.csv('data/dados_2014.csv')
 
     // Mostra pessoas candidatas
-    mostrar(dados)
+    filtrar()
 }
 
 // Chama função que carrega dados
